339d959a6640877eb5806016f5c43abcea5e5226
Sinan Shihab <sinan@192.168.0.199>
2022-10-20 17:31:26 +0530

    heroku

--- school_erp/settings.py
+++ school_erp/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.whitenoisemiddleware'
+    'whitenoise.middleware.whitenoisemiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
